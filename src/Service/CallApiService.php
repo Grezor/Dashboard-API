518b13187599e76c0b0d85363072ba756d829ce1
Geoffrey <grezor.gamer@gmail.com>
2021-10-27 20:45:08 +0200

    add issues

--- src/Service/CallApiService.php
+++ src/Service/CallApiService.php
@@ -2,7 +2,9 @@
 
 namespace App\Service;
 
+
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class CallApiService {
 
@@ -15,7 +17,23 @@ class CallApiService {
 
     public function getData(): array
     {
-        $response = $this->client->request('GET', 'https://api.github.com/users/grezor/repos?&type=all&direction=desc&per_page=100');
+        $response = $this->client->request(
+            'GET',
+            'https://api.github.com/users/grezor/repos?&type=all&direction=desc&per_page=100'
+        );
+        return $response->toArray();
+    }
+
+    public function getIssues(): array|Response
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://api.github.com/issues?state=all',
+            [
+                'auth_basic' => ['Grezor', $_ENV['KEY_GITHUB'] ],
+            ]
+
+        );
         return $response->toArray();
     }
 }
